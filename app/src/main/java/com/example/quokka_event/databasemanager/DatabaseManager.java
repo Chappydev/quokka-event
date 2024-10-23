a511010a16e2533b9a4795cc0e87fe6292fc2886
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-10-22 20:22:07 -0600

    Create databasemanager class

---
+++ app/src/main/java/com/example/quokka_event/databasemanager/DatabaseManager.java
@@ -0,0 +1,15 @@
+package com.example.quokka_event.databasemanager;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.FileInputStream;
+
+public class DatabaseManager {
+    private FirebaseFirestore database;
+    private CollectionReference ref;
+
+
+}
