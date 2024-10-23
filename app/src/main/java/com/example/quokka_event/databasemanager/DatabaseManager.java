947f32cb807d0920a0b7c8d9c98abe19a4adb0ed
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-10-22 20:28:50 -0600

    Add skeleton functions to DatabaseManager

--- app/src/main/java/com/example/quokka_event/databasemanager/DatabaseManager.java
+++ app/src/main/java/com/example/quokka_event/databasemanager/DatabaseManager.java
@@ -6,10 +6,28 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.FileInputStream;
-
+// Class to interact with the firebase database to be used for admin's screen
 public class DatabaseManager {
     private FirebaseFirestore database;
     private CollectionReference ref;
 
+    // delete event from firebase, make sure that it also deletes any data associated with it
+    public void deleteEvent(){
+
+    }
+
+    // delete profile from database.
+    public void deleteProfile(){
+
+    }
+
+    // Delete hashed qr code data
+    public void deleteQRCode(){
+
+    }
+    // Delete a facility delete events that are associated it with it
+    public void deleteFacility(){
+
+    }
 
 }
