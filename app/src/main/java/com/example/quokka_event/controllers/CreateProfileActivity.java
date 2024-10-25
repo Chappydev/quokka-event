c2638463d43d672b3035daddef5f80fc2b087f14
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-10-25 13:53:50 -0600

    Connect CreateProfileActivity to create_profile.xml

--- app/src/main/java/com/example/quokka_event/controllers/CreateProfileActivity.java
+++ app/src/main/java/com/example/quokka_event/controllers/CreateProfileActivity.java
@@ -1,6 +1,29 @@
 package com.example.quokka_event.controllers;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.quokka_event.R;
+
 public class CreateProfileActivity extends AppCompatActivity {
+
+    EditText editName;
+    EditText editEmail;
+    EditText editPhoneNumber;
+    Button confirmButton;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.create_profile);
+        editName = findViewById(R.id.edit_name);
+        editEmail = findViewById(R.id.edit_email);
+        editPhoneNumber = findViewById(R.id.edit_phone);
+        confirmButton = findViewById(R.id.confirm_button);
+    }
 }
