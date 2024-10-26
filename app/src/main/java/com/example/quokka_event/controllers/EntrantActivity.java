35221cd2d48b015fc300c99a7f84154c5d5f5688
soaibanur <117605312+soaibanur@users.noreply.github.com>
2024-10-25 23:41:54 -0600

    Update EntrantActivity.java

--- app/src/main/java/com/example/quokka_event/controllers/EntrantActivity.java
+++ app/src/main/java/com/example/quokka_event/controllers/EntrantActivity.java
@@ -1,24 +1,7 @@
 package com.example.quokka_event.controllers;
 
-// Function allows Entrant to interact with Event objects
+package com.example.quokka_event.controllers;
 
 public class EntrantActivity {
 
-    // US 1.1.1
-    // Soaiba and Simon
-    public void joinWaitlist() {
-
-    }
-
-    // US 1.5.2
-    // Soaiba and Simon
-    public void acceptInvite(){
-
-    }
-
-    // US 1.5.3
-    // Soaiba and Simon
-    public void declineInvite(){
-
-    }
 }
