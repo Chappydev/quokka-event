e4d3085565642442efda88eeb36ef1435f871e04
soaibanur <nursoaiba@gmail.com>
2024-10-25 23:20:00 -0600

    Skeleton for EntrantActivity

---
+++ app/src/main/java/com/example/quokka_event/controllers/EntrantActivity.java
@@ -0,0 +1,24 @@
+package com.example.quokka_event.controllers;
+
+// Function allows Entrant to interact with Event objects
+
+public class EntrantActivity {
+
+    // US 1.1.1
+    // Soaiba and Simon
+    public void joinWaitlist() {
+
+    }
+
+    // US 1.5.2
+    // Soaiba and Simon
+    public void acceptInvite(){
+
+    }
+
+    // US 1.5.3
+    // Soaiba and Simon
+    public void declineInvite(){
+
+    }
+}
