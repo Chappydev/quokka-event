ba68983861dfba6b140b4898adc389ed156bef38
ChappyDev <danjtchapman@gmail.com>
2024-10-26 20:38:26 -0600

    Make DatabaseManage a singleton; add initialize method

--- app/src/main/java/com/example/quokka_event/controllers/DatabaseManager.java
+++ app/src/main/java/com/example/quokka_event/controllers/DatabaseManager.java
@@ -1,13 +1,42 @@
 package com.example.quokka_event.controllers;
 
+import android.content.Context;
+
+import com.example.quokka_event.models.User;
+import com.example.quokka_event.models.admin.ProfileSystem;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 // Class to interact with the firebase database to be used for admin's screen
 public class DatabaseManager {
-    private FirebaseFirestore database;
-    private CollectionReference ref;
+    private FirebaseFirestore db;
+    private CollectionReference usersRef;
+    private CollectionReference facilityRef;
+    private CollectionReference eventsRef;
+    private Context applicationContext;
+    private static DatabaseManager instance;
 
+    private DatabaseManager(Context context) {
+        this.applicationContext = context.getApplicationContext();
+    }
+
+    // This will always return the same instance anywhere in the code
+    public static DatabaseManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new DatabaseManager(context);
+            instance.initialize();
+        }
+        return instance;
+    }
+
+    // Initialize all attributes and return the current instance
+    private DatabaseManager initialize() {
+        db = FirebaseFirestore.getInstance();
+        usersRef = db.collection("Users");
+        facilityRef = db.collection("Facility");
+        eventsRef = db.collection("Events");
+        return this;
+    }
     // delete event from firebase, make sure that it also deletes any data associated with it
     public void deleteEvent(){
 
