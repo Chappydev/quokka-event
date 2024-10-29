b7f3c0cec2880bdcd8bef920a3475b95bb6f6fbc
soaibanur <nursoaiba@gmail.com>
2024-10-29 12:05:14 -0600

    madejoinWaitlist() in EventManager and added javadocs

--- app/src/main/java/com/example/quokka_event/models/entrant/EventManager.java
+++ app/src/main/java/com/example/quokka_event/models/entrant/EventManager.java
@@ -11,9 +11,13 @@ import java.util.Date;
 // we deal with all the interactions between User and Event (joining waitlists, responding to
 // invites, etc.)
 public class EventManager {
+    private Event event;
+    private ProfileSystem profile;
 
     // Constructor
-    public EventManager() {
+    public EventManager(Event event, ProfileSystem profile) {
+        this.event = event;
+        this.profile = profile;
     }
 
     // Refactor: this can stay here as long as this class doesn't become coupled to a single Event
@@ -24,10 +28,14 @@ public class EventManager {
         // needs implementation
     }
 
-
-    public boolean joinWaitlist(Event event, ProfileSystem user) {
-        boolean waitlisted = event.addEntrantToWaitlist(user);
-        return waitlisted;
+    /**
+     * Adds entrant's profile to the waitlist for an Event.
+     * @author Soaiba
+     * @param event the event to be added to the waitlist for
+     * @param profile the entrant's profile to add to the waitlist
+     */
+    public void joinWaitlist(Event event, ProfileSystem profile) {
+        event.addEntrantToWaitlist(profile);
     }
 
     public void cancelWaitlistSpot(){
