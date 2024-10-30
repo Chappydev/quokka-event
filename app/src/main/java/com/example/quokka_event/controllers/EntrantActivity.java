4f2ce41e785bcb5bfb8948718e280aceac6d1fb9
soaibanur <nursoaiba@gmail.com>
2024-10-30 15:30:12 -0600

    changed entrant activity

--- app/src/main/java/com/example/quokka_event/controllers/EntrantActivity.java
+++ app/src/main/java/com/example/quokka_event/controllers/EntrantActivity.java
@@ -12,11 +12,6 @@ import com.example.quokka_event.R;
 import com.example.quokka_event.models.entrant.EventManager;
 
 public class EntrantActivity extends AppCompatActivity {
-    private EventManager eventManager;
-
-    private TextView waitlistJoinConfirmation;
-    private Button exitButton;
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
