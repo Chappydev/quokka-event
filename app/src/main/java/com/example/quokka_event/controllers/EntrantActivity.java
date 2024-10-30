b66c0c4fb248098c4917b6e5c96d30af97795bd9
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-10-30 14:09:19 -0600

    Add textviews to EntrantActivity

--- app/src/main/java/com/example/quokka_event/controllers/EntrantActivity.java
+++ app/src/main/java/com/example/quokka_event/controllers/EntrantActivity.java
@@ -2,6 +2,7 @@ package com.example.quokka_event.controllers;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +10,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.quokka_event.R;
 
 public class EntrantActivity extends AppCompatActivity {
+    private TextView eventTitle;
+    private TextView dateText;
+    private TextView timeText;
+    private TextView locationText;
+    private TextView organizerText;
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.eventlist_activity);
+        setContentView(R.layout.event_waitlist_details);
     }
 }
