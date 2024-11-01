134a00541b79872e0851d60bec511d69c46f33c7
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-10-31 19:48:40 -0600

    create myeventspagefragment

--- app/src/main/java/com/example/quokka_event/MainActivity.java
+++ app/src/main/java/com/example/quokka_event/MainActivity.java
@@ -4,28 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.quokka_event.controllers.DatabaseManager;
 import com.example.quokka_event.controllers.WaitlistActivity;
 import com.example.quokka_event.controllers.dbutil.DbCallback;
 import com.example.quokka_event.models.User;
 import com.example.quokka_event.models.ProfileSystem;
+import com.example.quokka_event.models.event.MyEventsPageFragment;
 
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button myEventButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.user_landing_page);
+
+        myEventButton = findViewById(R.id.my_events_button);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.landing_page), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -62,7 +70,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Switch activity to WaitlistActivity TEMPORARY FOR TESTING WAITLIST ACTIVITY.
     private void switchActivities(){
-        Intent switchActivityIntent = new Intent(this, WaitlistActivity.class);
-        startActivity(switchActivityIntent);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.my_events_page, new MyEventsPageFragment());
+        fragmentTransaction.addToBackStack(null); // Optional
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/quokka_event/models/event/MyEventsPageFragment.java
@@ -0,0 +1,22 @@
+package com.example.quokka_event.models.event;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.quokka_event.R;
+
+public class MyEventsPageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.user_my_events_page, container, false);
+    }
+}
