051182a925e58f7eaa12d20c027c49abd76360f6
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-11-02 16:55:15 -0600

    add CreateEventActivity.java

---
+++ app/src/main/java/com/example/quokka_event/CreateEventActivity.java
@@ -0,0 +1,6 @@
+package com.example.quokka_event;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateEventActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/example/quokka_event/OrganizerEventsPageActivity.java
+++ app/src/main/java/com/example/quokka_event/OrganizerEventsPageActivity.java
@@ -1,14 +1,12 @@
 package com.example.quokka_event;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.quokka_event.R;
 
 public class OrganizerEventsPageActivity extends AppCompatActivity {
     Button addButton;
@@ -20,14 +18,20 @@ public class OrganizerEventsPageActivity extends AppCompatActivity {
         addButton = findViewById(R.id.add_button_bottom);
         // Set up a click listener for the back button
         Button backButton = findViewById(R.id.back_button_bottom);
+
         backButton.setOnClickListener(v -> {
             finish();
         });
 
         addButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Launch CreateEventActivity to create event
+             * @param view
+             */
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(OrganizerEventsPageActivity.this, CreateEventActivity.class);
+                startActivity(intent);
             }
         });
     }
