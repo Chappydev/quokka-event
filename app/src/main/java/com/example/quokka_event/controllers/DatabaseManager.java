366b508128e197abbdd2a8895a68c91bd9ca3621
sdnair <sohannair25@gmail.com>
2024-11-04 12:32:56 -0700

    added database functions

--- app/src/main/java/com/example/quokka_event/controllers/DatabaseManager.java
+++ app/src/main/java/com/example/quokka_event/controllers/DatabaseManager.java
@@ -7,6 +7,8 @@ import androidx.annotation.NonNull;
 
 import com.example.quokka_event.controllers.dbutil.DbCallback;
 import com.example.quokka_event.models.ProfileSystem;
+import com.example.quokka_event.models.event.Event;
+import com.example.quokka_event.models.organizer.Facility;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
@@ -47,8 +49,6 @@ public class DatabaseManager {
         return this;
     }
 
-    // vvvv This function should be split into 2. The create user part should be its own function
-    // I made a split this function for better readability see createProfile()
     /**
      * This function will either find the user or if it doesn't exist yet, it will create a new user
      * with the deviceId.
@@ -77,12 +77,39 @@ public class DatabaseManager {
                         userData.put("admin", document.get("isAdmin"));
                         cb.onSuccess(userData);
 
-                    // Successful search but no such user
+                        // Successful search but no such user
                     } else {
-                        createProfile(cb,deviceId);
+                        // So we will create the user instead:
+                        Map<String, Object> userInfo = new HashMap<>();
+                        userInfo.put("name", "");
+                        userInfo.put("email", "");
+                        userInfo.put("address", "");
+                        userInfo.put("isOrganizer", false);
+                        userInfo.put("isAdmin", false);
+                        usersRef.document(deviceId).set(userInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // Successfully created a user with default values
+                                if (task.isSuccessful()) {
+                                    // Set up the data and pass it into the callback
+                                    Map<String, Object> userData = new HashMap<>();
+                                    userData.put("deviceID", deviceId);
+                                    userData.put("profile", new ProfileSystem());
+                                    userData.put("organizer", false);
+                                    userData.put("admin", false);
+                                    cb.onSuccess(userData);
+
+                                    // There was an error trying to create the new user
+                                } else {
+                                    Log.e("DB", "Write for User: something went wrong creating the new user", task.getException());
+                                    // Can handle this error in the UI through the onError callback
+                                    cb.onError(task.getException());
+                                }
+                            }
+                        });
                     }
 
-                // There was an error when trying to get the user data:
+                    // There was an error when trying to get the user data:
                 } else {
                     Log.e("DB", "Query for User: something went wrong", task.getException());
                     // Can handle this error in the UI through the onError callback
@@ -122,84 +149,73 @@ public class DatabaseManager {
         return profile;
     }
 
-    /**
-     * Creates a new profile in the firestore database
-     * @author Chappydev
-     * @param cb
-     * @param deviceId
-     */
-    public void createProfile(DbCallback cb, String deviceId){
-        // So we will create the user instead:
-        Map<String, Object> userInfo = new HashMap<>();
-        userInfo.put("name", "");
-        userInfo.put("email", "");
-        userInfo.put("address", "");
-        userInfo.put("isOrganizer", false);
-        userInfo.put("isAdmin", false);
-        usersRef.document(deviceId).set(userInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                // Successfully created a user with default values
-                if (task.isSuccessful()) {
-                    // Set up the data and pass it into the callback
-                    Map<String, Object> userData = new HashMap<>();
-                    userData.put("deviceID", deviceId);
-                    userData.put("profile", new ProfileSystem());
-                    userData.put("organizer", false);
-                    userData.put("admin", false);
-                    cb.onSuccess(userData);
+    // Add event document to firestore
+    public void addEvent(Event event, DbCallback callback){
+        Map<String, Object> eventPayload = new HashMap<>();
+        eventPayload.put("eventId", event.getEventID());
+        eventPayload.put("eventName", event.getEventName());
+        eventPayload.put("eventDate", event.getEventDate());
+        eventPayload.put("eventLocation", event.getEventLocation());
+        eventPayload.put("registrationDeadline", event.getRegistrationDeadline());
+        eventPayload.put("maxParticipants", event.getMaxParticipants());
+        eventPayload.put("maxWaitlist", event.getMaxWaitlist());
 
-                    // There was an error trying to create the new user
-                } else {
-                    Log.e("DB", "Write for User: something went wrong creating the new user", task.getException());
-                    // Can handle this error in the UI through the onError callback
-                    cb.onError(task.getException());
-                }
-            }
-        });
-    }
-
-    /**
-     * Updates a profile in the database
-     * @author saimonnk
-     * @param profile
-     */
-    public void updateProfile(ProfileSystem profile){
-        String deviceId = profile.getDeviceID();
-        Map<String, Object> userInfo = new HashMap<>();
-        userInfo.put("name", profile.getName());
-        userInfo.put("email", profile.getEmail());
-        userInfo.put("address", profile.getAddress());
-        usersRef.document(deviceId).set(userInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
-                    Log.d("DB","Profile updated");
-                }
-                else {
-                    Log.e("DB", "Unable to update profile", task.getException());
-                }
-            }
-        });
+        // Pushing the payload to the collection
+        eventsRef
+                .add(eventPayload)
+                .addOnSuccessListener(documentReference -> {
+                    documentReference.update("eventId", documentReference.getId())
+                            .addOnSuccessListener(response -> callback.onSuccess(documentReference.getId()))
+                            .addOnFailureListener(exception -> callback.onError(exception));
+                })
+                .addOnFailureListener(exception -> callback.onError(exception));
     }
 
 
-    // delete event from firebase, make sure that it also deletes any data associated with it
-    public void deleteEvent(){
-
+    // delete event from firestore, making sure that it also deletes any data associated with it
+    public void deleteEvent(String eventId, DbCallback callback){
+        eventsRef
+                .document(eventId)
+                .delete()
+                .addOnSuccessListener(response -> callback.onSuccess(response))
+                .addOnFailureListener(exception -> callback.onError(exception));
     }
 
     // delete profile from database.
-    public void deleteProfile(){
-
+    public void deleteProfile(String deviceId, DbCallback callback){
+        usersRef
+                .document(deviceId)
+                .delete()
+                .addOnSuccessListener(response -> callback.onSuccess(response))
+                .addOnFailureListener(exception -> callback.onError(exception));
     }
 
+    // Adds a facility profile to firestore
+    public void addFacility(Facility facility , DbCallback callback){
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("facilityName", facility.getFacilityName());
+        payload.put("facilityLocation", facility.getFacilityLocation());
+
+        facilityRef
+                .add(payload)
+                .addOnSuccessListener(DocumentReference -> {
+                    DocumentReference.update("facilityId", DocumentReference.getId())
+                            .addOnSuccessListener(v -> callback.onSuccess(DocumentReference.getId())) // TODO: possible change return value to response object post testing.
+                            .addOnFailureListener(exception -> callback.onError(exception));
+                })
+                .addOnFailureListener(exception -> callback.onError(exception));
+    }
+
+    // Delete a facility delete events that are associated it with it
+    public void deleteFacility(String facilityId, DbCallback callback){
+        facilityRef
+                .document(facilityId)
+                .delete()
+                .addOnSuccessListener(response -> callback.onSuccess(response))
+                .addOnFailureListener(exception -> callback.onError(exception));
+    }
     // Delete hashed qr code data
     public void deleteQRCode(){
-
-    }
-    // Delete a facility delete events that are associated it with it
-    public void deleteFacility(){
 
     }
 
