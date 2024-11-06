6280844e9070f2253b8c7975ede561bfdb481ee8
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-11-06 15:56:58 -0700

    make skeleton of getallprofiles in databasemanager.java

--- app/src/main/java/com/example/quokka_event/ProfileSystem.java
+++ app/src/main/java/com/example/quokka_event/ProfileSystem.java
@@ -89,4 +89,5 @@ public class ProfileSystem {
         return address;
     }
 
+
 }
--- app/src/main/java/com/example/quokka_event/controllers/DatabaseManager.java
+++ app/src/main/java/com/example/quokka_event/controllers/DatabaseManager.java
@@ -14,7 +14,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -167,4 +170,22 @@ public class DatabaseManager {
 
     }
 
+    public void getAllProfiles(){
+        ArrayList<ProfileSystem> users = new ArrayList<ProfileSystem>();
+        usersRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                ProfileSystem user = new ProfileSystem();
+
+
+                            }
+                        } else{
+
+                        }
+                    }
+                });
+    }
 }
