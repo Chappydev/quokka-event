2702e6542b86af5777f1279fb039af7f06cd3339
Soaiba Nur <nursoaiba@gmail.com>
2024-11-06 17:07:42 -0700

    javadocs for confirmation activity

--- app/src/main/java/com/example/quokka_event/controllers/ConfirmationActivity.java
+++ app/src/main/java/com/example/quokka_event/controllers/ConfirmationActivity.java
@@ -2,25 +2,29 @@ package com.example.quokka_event.controllers;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quokka_event.R;
 
+/**
+ * This class displays a message based on what button the user interacted with from the events detail page.
+ * Handles activity for the exit button.
+ * @author Soaiba
+ */
 public class ConfirmationActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_confirmation);
 
-        // Message to display
         String messageType = getIntent().getStringExtra("message_type");
         String eventName = getIntent().getStringExtra("event_name");
+
         TextView confirmationMessage = findViewById(R.id.confirmationMessage);
 
-        // Set the message based on the message type
+        // Display the message based on the message type
         if ("Accept".equals(messageType)) {
             confirmationMessage.setText("You have joined " + eventName + "!");
         } else if ("Deny".equals(messageType)) {
@@ -29,17 +33,18 @@ public class ConfirmationActivity extends AppCompatActivity{
             confirmationMessage.setText("You have unjoined the waitlist to " + eventName + ".");
         }
 
-        // Exit button activity
-        // Returns to event list page
+        /**
+         * Method handles exit function activity.
+         * Returns user to event list page.
+         * @see https://medium.com/@snaresh22/mastering-android-app-navigation-with-intent-flags-36f84409432b
+         * @author Soaiba
+         */
         Button exitButton = findViewById(R.id.exitButton);
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ConfirmationActivity.this, MyEventsActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                finish();
-            }
+        exitButton.setOnClickListener(v -> {
+            Intent intent = new Intent(ConfirmationActivity.this, MyEventsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
         });
     }
 }
