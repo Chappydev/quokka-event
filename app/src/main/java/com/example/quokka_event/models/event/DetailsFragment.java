6a32e081cfb569aa0379e2f3b6b83231e1606d88
speakerchef <sohannair25@gmail.com>
2024-11-08 14:36:30 -0700

    Fixed waitlist and participant limit implementation

--- app/src/main/java/com/example/quokka_event/models/event/DetailsFragment.java
+++ app/src/main/java/com/example/quokka_event/models/event/DetailsFragment.java
@@ -2,7 +2,9 @@ package com.example.quokka_event.models.event;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,20 +13,22 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.quokka_event.R;
+import com.example.quokka_event.controllers.EventTabsActivity;
 
 public class DetailsFragment extends Fragment {
+
     TextView remainSeatTextView;
     Button changeSeatButton;
     Button confirmChangeSeatButton;
-
     CheckBox limitWaitlistCheckBox;
     EditText waitlistCapEditText;
-
     CheckBox limitParticipantCheckBox;
     EditText participantCapEditText;
     Boolean isWaitlistLimit;
@@ -49,6 +53,16 @@ public class DetailsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d("FragmentDebug", "Context class: " + context.getClass().getName());
+        if(context instanceof OverviewFragment.overviewEditListener){
+            listener = (DetailsFragment.detailsListener) context;
+        } else {
+            throw new RuntimeException(context + "must implement overeditListener");
+        }
+    }
     /**
      * Attach the detailsListener listener to EventTabsActivity.java
      * @param context
@@ -144,21 +158,24 @@ public class DetailsFragment extends Fragment {
                 // forgive me for the if else mess
                 String maxParticipant = participantCapEditText.getText().toString();
                 String maxWaitlist = waitlistCapEditText.getText().toString();
+
                 if ( (maxParticipant.isEmpty() && limitParticipantCheckBox.isChecked()) ||
                         (maxWaitlist.isEmpty() && limitWaitlistCheckBox.isChecked()) ){
                     displayWarning("Please enter a number");
                     return;
                 }
                 if (limitWaitlistCheckBox.isChecked()){
+                    participantLimit = Integer.parseInt(maxParticipant);
                     waitlistLimit = Integer.parseInt(maxWaitlist);
-                    if (waitlistLimit < currentNumWaitlist){
+                    if (participantLimit < waitlistLimit){
                         displayWarning("Cannot set capacity lower than number of people in waitlist!");
                         return;
                     }
                 }
                 if (limitParticipantCheckBox.isChecked()){
+                    waitlistLimit = Integer.parseInt(maxWaitlist);
                     participantLimit = Integer.parseInt(maxParticipant);
-                    if (participantLimit < currentNumParticipants){
+                    if (participantLimit < waitlistLimit){
                         displayWarning("Cannot set capacity lower than current number of people registered!");
                         return;
                     }
@@ -168,21 +185,24 @@ public class DetailsFragment extends Fragment {
                 if (!limitParticipantCheckBox.isChecked()) {
                     participantLimit = Integer.MAX_VALUE;
                     remainSeatTextView.setText("Max");
+                    Toast.makeText(getContext(), "capacity updated", Toast.LENGTH_LONG).show();
                     return;
                 }
-                changeSeatButton.setVisibility(View.VISIBLE);
                 int remainingSeat = participantLimit - currentNumParticipants;
                 remainSeatTextView.setText(Integer.toString(remainingSeat));
                 listener.setCapacity(waitlistLimit, participantLimit);
-                setButtonsVisibility(View.GONE);
+                Toast.makeText(getContext(), "capacity updated", Toast.LENGTH_LONG).show();
+//                setButtonsVisibility(View.INVISIBLE);
             }
+
         });
-
-
         // Inflate the layout for this fragment
         return view;
     }
 
+    public interface CapacityUpdateListener {
+        void onCapacityUpdated(int waitlistCap, int partCap);
+    }
     /**
      * Set the visibility of change seat components
      * @param v the visibility
