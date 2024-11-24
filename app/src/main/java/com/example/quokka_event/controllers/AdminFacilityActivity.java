5052ed73643f242e577a4c294b363e091db8a1c0
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-11-23 22:56:04 -0700

    add facility activity

--- app/src/main/java/com/example/quokka_event/controllers/AdminFacilityActivity.java
+++ app/src/main/java/com/example/quokka_event/controllers/AdminFacilityActivity.java
@@ -6,9 +6,12 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.quokka_event.R;
+
 public class AdminFacilityActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_facility_activity);
     }
 }
