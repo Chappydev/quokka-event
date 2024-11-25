2915b0d556ecd4be3f2d8f9f69f00a1274411b61
mylayambao <91172629+mylayambao@users.noreply.github.com>
2024-11-25 13:14:10 -0700

    notification class

---
+++ app/src/main/java/com/example/quokka_event/models/Notification.java
@@ -0,0 +1,10 @@
+package com.example.quokka_event.models;
+
+public class Notification {
+    public String notifTitle;
+    public String notifDescription;
+    private String eventID;
+    private String eventName;
+}
+
+
